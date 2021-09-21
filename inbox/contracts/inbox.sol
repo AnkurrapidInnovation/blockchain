c4b702d96e723e110430445f581dcede248a21cc
ankur <ankur.sh2011@gmail.com>
2021-09-21 16:05:45 +0530

    lottery and inbox project

--- inbox/contracts/inbox.sol
+++ inbox/contracts/inbox.sol
@@ -1,17 +1,39 @@
-pragma solidity ^0.4.17; // Pragma. The first line is a pragma directive
+pragma solidity ^0.8.7; // Pragma. The first line is a pragma directive
  //which tells that the source code is written for Solidity version 0.4. 0 or anything newer that does not break functionality up to, 
  //but not including, version 0.6.
 
 contract Inbox {
     string public message;
 
-    function Inbox (string initialMessage) public {
-        message = initialMessage;
-    }
+    // function Inbox (string initialMessage) public {
+    //     message = initialMessage;
+    // }
 
-    function setMessage (string newMessage) public {
+    constructor(string initialMessage) public {
+    message = initialMessage;
+}
+
+    // function setMessage (string newMessage) public {
+    //     message = newMessage;
+
+
+    // }
+
+     constructor (string newMessage) public {
         message = newMessage;
+
 
     }
  
-}
+ }  
+
+
+// contract Lottery {
+
+//     address public manager ;
+
+//     function Lottery() public {+
+//  manager = msg.sender;
+
+//     }
+// }
