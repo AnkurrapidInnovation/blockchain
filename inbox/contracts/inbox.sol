b842db95677b3599203cc7728c450f4d6a3f474b
ankur <ankur.sh2011@gmail.com>
2021-09-22 18:55:52 +0530

    checking all the bugs and resolve it

--- inbox/contracts/inbox.sol
+++ inbox/contracts/inbox.sol
@@ -1,39 +1,17 @@
-pragma solidity ^0.4.17; // Pragma. The first line is a pragma directive
- //which tells that the source code is written for Solidity version 0.4. 0 or anything newer that does not break functionality up to, 
- //but not including, version 0.6.
+pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
 
-    // function Inbox (string initialMessage) public {
-    //     message = initialMessage;
-    // }
-
-    constructor(string initialMessage) public {
-    message = initialMessage;
-}
-
-    // function setMessage (string newMessage) public {
-    //     message = newMessage;
-
-
-    // }
-
-     constructor (string newMessage) public {
-        message = newMessage;
-
-
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
     }
- 
- }  
-
-
-// contract Lottery {
-
-//     address public manager ;
-
-//     function Lottery() public {+
-//  manager = msg.sender;
-
-//     }
-// }
+    
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+    
+    function getMessage()public view returns (string) {
+        return message;
+    }
+}
