8e5880a0b662e46d71ff962f511555577dc61af7
ankur <ankur.sh2011@gmail.com>
2021-09-24 19:26:42 +0530

    deploying problem resolved

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { interface, bytecode ,abi} = require('./compile');
 
 const provider = new HDWalletProvider(
   'jump crucial chunk nature orchard destroy waste group hidden helmet gather excess',
@@ -13,9 +13,9 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data:  bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '5000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
 };
